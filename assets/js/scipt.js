1ed16ebc5a8f7cc3bc5b658035d89dd033a21213
Philippe Recto <precto1285@gmal.com>
2018-07-13 11:09:43 -0700

    added functions for operators

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -61,6 +61,32 @@ $(document).ready(function () {
         //Set isCalculated to true
         isCalculated = true;
 
+        //Use parseInt to convert our string representation of numbers into intergers
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+
+        //Operator choice
+        //Run the operation and set the HTML of the result
+        if(operator === "plus") {
+            result = firstNumber + secondNumber;
+        }
+        else if(operator === "minus") {
+            result = firstNumber - secondNumber;
+        }
+        else if(operator === "times") {
+            result = firstNumber * secondNumber;
+        }
+        else if(operator === "divide") {
+            result = firstNumber / secondNumber;
+        }
+        else if(operator === "power"){
+            result = furstNumber ^ secondNumber;
+        }
+
+        $("#result").text(result);
+
+    }).on("click", function() {
+
         
     })
 
