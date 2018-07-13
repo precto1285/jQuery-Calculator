b1ff3688a6dd10d49ef3cabbbc8af7d462908e7e
Philippe Recto <precto1285@gmal.com>
2018-07-13 11:02:59 -0700

    more functions for calculator

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,8 +1,8 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Global Variables
-    var firstNumber= 0;
+    var firstNumber = 0;
     var secondNumber = 0;
     var operator = "";
     var result = 0;
@@ -16,8 +16,52 @@ $(document).ready(function() {
         operator = "";
         isOperatorChosen = false;
         isCalculated = false;
-        
+
         $("#first-number, #second-number, #operator, #result").empty();
     }
+
+    $(".card").on("click", ".number", function () {
+
+        //Check if already run a calculation
+        if (isCalculated) {
+            return false;
+        }
+
+        //If operator is chose, write the secondNumber, otherwise, the firstNumber
+        if (isOperatorChosen) {
+            secondNumber += $(this).val();
+            $("#second-number").text(secondNumber);
+        }
+        else {
+            firstNumber += $(this).val();
+            $("#first-number").text(firstNumber);
+        }
+    }).on("click", ".operator", function () {
+
+        // Check if first number has been selected
+        // Or we've already run a calculation, if so, exit
+        if(!firstNumber || isCalculated) {
+            return false;
+        }
+
+        //Set isOperatorChosen to true to start writing secondNumber
+        isOperatorChosen = true;
+
+        //Store off the operator
+        operator = $(this).val();
+
+        //Set the html of the #operator to the text of what was clicked
+        $("#operator").text($(this).text());
+    }).on("click", ".equal",function() {
+        //If we already clicked equal, do not run calculation again
+        if(isCalculated) {
+            return false;
+        }
+
+        //Set isCalculated to true
+        isCalculated = true;
+
+        
+    })
 
 });
